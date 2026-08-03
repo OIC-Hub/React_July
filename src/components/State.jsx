893dd2f2d8b099bd845eeefa76944c06ec2d8575
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-08-03 10:29:48 +0100

    updated

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function State(){
     const [count, setCount] = useState(0);
+
+
+    // useEffect(() => {
+    //     document.title = "your counts is " + count
+    // })
+
+    useEffect(() => {
+    //  document.title =  "hola! welcome"
+        document.title = "your counts is " + count
+
+    }, [count])
 
     return(
         <>
         <p>{count}</p>
         <button onClick={() => setCount(count + 1)}>Add Counts</button>
-
+        {/* <h1>{welcomeMsg}</h1> */}
         </>
     )
 }
